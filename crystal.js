340b6bc6fec426111026a7b55e65db4733eb5bc0
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-07-23 09:34:57 -0400

    started my jquery

--- crystal.js
+++ crystal.js
@@ -1,5 +1,10 @@
+
+//Random diamonds Numbers
+var diamondRandom = [Math.floor(Math.random()* 12) +1];
+//Hide diamonds variables
+
 //Guess
-var guess = 45;
+var guess = 0;
 $("#guess").text(guess);
 //Score
 var score = 0;
@@ -9,10 +14,31 @@ console.log("Current Score is: " + score);
 var guessRandom = [Math.floor(Math.random()* 101) +19];
 //Shows score on page
 $("#guess").text(guessRandom);
-console.log()
-//Diamond Buttons Array
-var diamondVar [10, 5, 3, 7];
+console.log("Guessed Number is: " + guess);
+//Win Box
+var winBox = [];
+$("#win-box").text(wins);
+console.log("Number of wins: " + wins);
+//Loss Box
+var lossBox = [];
+$("#loss-box").text(losses);
+console.log("Number of losses: " + losses);
+//Win function
+function wins() {
+    wins++;
+    $("#win-box").text(wins);
+}
+//Loss function
+function losses() {
+    losses++;
+    $("#loss-box").text(losses);
+}
 
+
+//Create a loop for each crytal
+for (var i = 0; i < guessRandom.length; i++){
+
+}
 //Create a Loop for Each crytasal
 for (var i = 0; i < diamondVar.length; i++) {
 //Give Img variable
@@ -24,16 +50,20 @@ var imageDiamond = $("<img>");
         // $("p").text("Hello World!")
     // });
 // });
-$("#button1").on("click", function(){}
+$("#button1").on("click", function(){
+    score = parseInt(score) + parseInt(diamond1);
+    $()
+}
 $("#button2").on("click", function(){}
 $("#button3").on("click", function(){}
 $("#button4").on("click", function(){}
 
-var scope = {};
-for(var i = 0; i < list.lenght; i++){ 
-  scope['counter' + i] = 0;   // create scope.counter1, scope.counter2,...)
+//Declare Winn
+if (guessRandom === score) {
+    wins();
+    console.log("You Won!")
 }
-with(scope){
- // here you can acesess keys in the scope object like them variable on the function scope
- counter0++
+else if (guessRandom < score) {
+    losses();
+    console.log("You Lose!")
 }
