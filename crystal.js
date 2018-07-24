8f9cab161451674c7f1cc8a35c6c12e84e9c0cb1
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-07-24 01:02:39 -0400

    saving for homework

--- crystal.js
+++ crystal.js
@@ -1,45 +1,74 @@
 var randomDiamond;
-var losses;
-var wins;
+var loss = 0;
+var win = 0;
+var score = 0;
 
-randomDiamond = Math.floor(Math.random() * 101 ) +19;
-console.log(randomDiamond);
+//This begins the game and restarts after win or loss
+var resetAndStartGame = function () {
 
-//Guess will generate a new random number
+    randomDiamond = Math.floor(Math.random() * 101) + 19;
+    console.log(randomDiamond);
 
-// $(("#guess").html("<h2>randomDiamond</h2>"));
+    //Guess will generate a new random number
 
-//4 Diamonds that produce random results
-for (var i = 0; i < 4; i++){
+    // $(("#guess").html("<h2>randomDiamond</h2>"));
 
-//Every diamond will have a random number between 1-12
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
+    //4 Diamonds that produce random results
+    for (var i = 0; i < 4; i++) {
 
-//A new random number will generate every time we win or lose 
-    var diamonds = $("<div>");
-    diamonds.attr({
-        "class": 'diamonds',
-        "data-random": random
-    });
+        //Every diamond will have a random number between 1-12
+        var random = Math.floor(Math.random() * 12);
+        console.log(random);
 
-    $(".diamonds").append(diamonds)
+        var diamonds = $("<div>");
+        diamonds.attr({
+            "class": 'diamonds',
+            "data-random": random
+        });
+
+        $(".diamonds").append(diamonds)
+    }
+}
+resetAndStartGame();
+
+//Reset
+var reset = function () {
+
 }
 
 //When clicking on the diamonds, it should add to the previous result Until it equals the random result
-$(".diamonds").on('click', function () {
-    // console.log($(this).attr('data-random')); 
-})
+$(document).on('click', ".diamonds", function () {
+    var num = parseInt($(this).attr('data-random'));
+    console.log(score);
+
+    score += num;
+
+
+    //If it is greater than the random result, we decrement the lost counter
+    if (score > randomDiamond) {
+        loss--;
+        $("#loss").html(loss);
+        score = 0 ;
+        resetAndStartGame();
+    }
+    //If it is equal then we increment the win counter
+    else if (score === randomDiamond) {
+        win++;
+        $("#win").html(win);
+        score = 0 ;
+        resetAndStartGame();
+    }
+
+
+
+});
+
+
+
+//A new random number will generate every time we win or lose 
 
 
 
 
 
 
-
-
-
-
-//If it is greater than the random result, we decrement the lost counter
-
-//If it is equal then we increment the win counter
