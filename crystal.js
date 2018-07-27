0120903ddadfb36248b3209786ec77f5a1165168
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-07-27 15:04:47 -0400

    made the crystal game working

--- crystal.js
+++ crystal.js
@@ -14,19 +14,20 @@ var resetAndStartGame = function () {
     $("#guess").html("<h2>" + randomDiamond + "</h2>");
 
     //4 Diamonds that produce random results
-    for (var i = 0; i < 4; i++) {
+    for (var i = 1; i < 5; i++) {
 
         //Every diamond will have a random number between 1-12
-        var random = Math.floor(Math.random() * 12);
-        console.log("this is a number" + random);
+        var random = Math.floor(Math.random() * 12) +1;
+        console.log("this is a number: " + random);
 
-        var diamonds = $("<div>");
-        diamonds.attr({
+        // var diamonds = $("<button>");
+        var btn = $("#btn" + i)
+        $(btn).attr({
             "class": 'diamonds',
             "data-random": random
         });
-        console.log("diamond object" +  diamonds);
-        $(".diamonds").append(diamonds)
+        console.log("diamond object" + btn);
+        // $(".diamonds").append(btn)
     }
 }
 resetAndStartGame();
@@ -38,25 +39,25 @@ var reset = function () {
 
 //When clicking on the diamonds, it should add to the previous result Until it equals the random result
 $(document).on('click', ".diamonds", function () {
-    var num = $(this).attr('data-random');
+    var num = parseInt($(this).attr('data-random'));
     console.log("this is the variable" + num);
 
-    score += num;
+    score = score += num;
     $("#score").text(score);
     console.log(score);
 
     //If it is greater than the random result, we decrement the lost counter
     if (score > randomDiamond) {
         loss++;
-        $("#loss").text(loss);
-        score = 0 ;
+        $("#loss").html("Losses: " + loss);
+        score = 0;
         resetAndStartGame();
     }
     //If it is equal then we increment the win counter
     else if (score === randomDiamond) {
         win++;
-        $("#win").text(win);
-        score = 0 ;
+        $("#win").html("Wins: " + win);
+        score = 0;
         resetAndStartGame();
     }
 
